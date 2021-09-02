38dcdf4d3ec5fdb997434ef09f13c26f2336bf5f
VictorTao1998 <jit124@ucsd.edu>
2021-09-02 09:03:03 -0700

    a

--- options/base_options.py
+++ options/base_options.py
@@ -3,7 +3,7 @@ import os
 from utils import util
 import torch
 import models
-import data
+#import data
 
 
 class BaseOptions():
@@ -133,9 +133,9 @@ class BaseOptions():
         opt, _ = parser.parse_known_args()  # parse again with new defaults
 
         # modify dataset-related parser options
-        dataset_name = opt.dataset_mode
-        dataset_option_setter = data.get_option_setter(dataset_name)
-        parser = dataset_option_setter(parser, self.isTrain)
+        #dataset_name = opt.dataset_mode
+        #dataset_option_setter = data.get_option_setter(dataset_name)
+        #parser = dataset_option_setter(parser, self.isTrain)
 
         # save and return the parser
         self.parser = parser
