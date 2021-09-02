d2928b80e77bb0c21a312183abad1d6d647c024e
VictorTao1998 <jit124@ucsd.edu>
2021-09-02 10:59:19 -0700

    a

--- models/__init__.py
+++ models/__init__.py
@@ -34,7 +34,7 @@ def get_option_setter(model_name):
     return model_class.modify_commandline_options
 
 
-def create_model(opt):
+def create_model(opt, model):
     """Create a model given the option.
 
     This function warps the class CustomDatasetDataLoader.
@@ -45,6 +45,6 @@ def create_model(opt):
         >>> model = create_model(opt)
     """
     model = find_model_using_name(opt.model)
-    instance = model(opt)
+    instance = model(opt, model)
     print("model [%s] was created" % type(instance).__name__)
     return instance
